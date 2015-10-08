80acdcca985f10cd072afb733321a6111c0d8fca
Andreas Lindberg <anlindbe@cisco.com>
2015-10-08 15:50:32 +0200

    La till en for-loop

--- pythonTest/src/pythonClass1/project1.py
+++ pythonTest/src/pythonClass1/project1.py
@@ -3,8 +3,12 @@ Created on Oct 8, 2015
 
 @author: andreaslindberg
 '''
-print 'Hello World!'
 
 print 'tjena'
 
+for x in range(0, 3):
+    print "We're on time %d" % (x)
+
 print 'OK'
+
+print'test'
